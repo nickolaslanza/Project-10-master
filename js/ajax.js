d4ecf7cd449c10142af382cc7dd2378b86cdb1c4
Nickolas Teixeira <NickolasTeixeira01@gmail.com>
2017-03-28 08:44:09 -0700

    small updates

--- js/ajax.js
+++ js/ajax.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
   let spotifyAPIfrankOcean = spotifyEndPoint + artists + '/' + frankOcean + '/' + albums;
 
   function displayList(data) {
-    let musicHTML = '<div class=".app-container">'
+    let musicHTML = '<div class=".app-container">';
     $.each(data.items, function(i, items){
       musicHTML += '<div class="album">';
       // why does items.artist[i].name not work?
@@ -22,11 +22,11 @@ $(document).ready(function() {
       musicHTML += '<p><a href="' + items.external_urls.spotify + '" target="_blank">' + items.name + '</a></p>';
       for (let k = 0; k < items.images.length; k++) {
         musicHTML += '<a href="' + items.images[0].url + '" data-lightbox="images" data-title="' + items.name + ': ' +  items.type.toUpperCase() + '">';
-        musicHTML += '<img src="' + items.images[0]['url'] + '">';
+        musicHTML += '<img src="' + items.images[0].url + '">';
         musicHTML += '</a>';
         break;
       }
-      musicHTML += '</div>'
+      musicHTML += '</div>';
     });
     musicHTML += '</div>';
     $('.api-container').html(musicHTML);
